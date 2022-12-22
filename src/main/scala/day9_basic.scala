e2dd9088bf0c3f3b1d3c6810f222533ee127011b
Adam Vandervorst <adam.vandervorst@gmail.com>
2022-12-23 00:17:18 +0100

    Finish day 9

--- src/main/scala/day9_basic.scala
+++ src/main/scala/day9_basic.scala
@@ -13,60 +13,41 @@ object Pos:
     case 'R' => (1, 0)
 
 extension (x: Pos)
-  def unary_- : Pos = (-x._1, -x._2)
+  def norm : Double = Math.sqrt(x._1*x._1 + x._2*x._2)
   def +(y: Pos): Pos = (x._1 + y._1, x._2 + y._2)
   def -(y: Pos): Pos = (x._1 - y._1, x._2 - y._2)
-  infix def dot(y: Pos): Int = x._1 * y._1 + x._2 * y._2
+  def /(n: Int): Pos = (x._1/n, x._2/n)
+
 
 val line_pattern = raw"([LUDR]) (\d+)".r
 def parse_line(line: String): (Pos, Int) = line match
   case line_pattern(dir, count) => Pos.fromLetter(dir.head) -> count.toInt
   case _ => throw RuntimeException(s"Invalid line: $line")
 
-def execute(instructions: Iterator[(Pos, Int)]): Iterator[Pos] =
-  var head_pos = (0, 0)
-  var relative_tail_pos = (0, 0)
+def execute(instructions: Iterator[(Pos, Int)], length: Int): Iterator[Pos] =
+  val positions = Array.fill(length)((0, 0))
 
   for case (offset, steps) <- instructions
-           _ <- 1 to steps yield
-    head_pos = head_pos + offset
-    relative_tail_pos = follow(relative_tail_pos, offset)
-    head_pos + relative_tail_pos
+      _ <- 1 to steps yield
 
-def follow(relative_tail_pos: Pos, offset: Pos): Pos =
-  // TH    =>
-  //  TH
-  // (-1, 0),  (1, 0)
-  // (-1, 0)
-  if relative_tail_pos == -offset then
-    relative_tail_pos
-  // TH    =>
-  // T
-  //  H
-  // (-1, 0),  (0, -1)
-  // (-1, 1)
-  //   T
-  //  H    =>
-  //   T
-  //   H
-  // (1, 1), (1, 0)
-  // (0, 1)
-  else if (relative_tail_pos dot offset) >= 0 then
-    relative_tail_pos - offset
-  // T
-  //  H    =>
-  //  TH
-  // (-1, 1),  (1, 0)
-  // (-1, 0)
-  else
-    -offset
+    positions(0) += offset
+    for i <- 1 until length do
+      positions(i) = positions(i - 1) + follow(positions(i) - positions(i - 1))
+
+    positions.last
+
+def follow(diff: Pos): Pos =
+  if diff.norm >= 2 then diff / 2 else diff
 
 
 @main def run_part1 =
   println(summarizeResource("day9_data.txt") { lines =>
-    val positions = execute(lines.map(parse_line)).toSet
+    val positions = execute(lines.map(parse_line), 2).toSet
     positions.size
   })
 
 @main def run_part2 =
-  ???
+  println(summarizeResource("day9_data.txt") { lines =>
+    val positions = execute(lines.map(parse_line), 10).toSet
+    positions.size
+  })
