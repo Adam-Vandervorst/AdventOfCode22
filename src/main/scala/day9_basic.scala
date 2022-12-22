e771a8a7b0fed531f961515236bb2b89e677cc43
Adam Vandervorst <adam.vandervorst@gmail.com>
2022-12-22 13:32:40 +0100

    Day 9 part 1 cleaned

--- src/main/scala/day9_basic.scala
+++ src/main/scala/day9_basic.scala
@@ -5,21 +5,12 @@ import aoc22.summarizeResource
 
 type Pos = (Int, Int)
 
-enum Direction:
-  case Left, Up, Down, Right
-
-  def offset: Pos = this match
-    case Left => (-1, 0)
-    case Up => (0, 1)
-    case Down => (0, -1)
-    case Right => (1, 0)
-
-object Direction:
-  def fromLetter(c: Char): Direction = c match
-    case 'L' => Left
-    case 'U' => Up
-    case 'D' => Down
-    case 'R' => Right
+object Pos:
+  def fromLetter(c: Char): Pos = c match
+    case 'L' => (-1, 0)
+    case 'U' => (0, 1)
+    case 'D' => (0, -1)
+    case 'R' => (1, 0)
 
 extension (x: Pos)
   def unary_- : Pos = (-x._1, -x._2)
@@ -28,25 +19,22 @@ extension (x: Pos)
   infix def dot(y: Pos): Int = x._1 * y._1 + x._2 * y._2
 
 val line_pattern = raw"([LUDR]) (\d+)".r
-def parse_line(line: String): (Direction, Int) = line match
-  case line_pattern(dir, count) => Direction.fromLetter(dir.head) -> count.toInt
+def parse_line(line: String): (Pos, Int) = line match
+  case line_pattern(dir, count) => Pos.fromLetter(dir.head) -> count.toInt
   case _ => throw RuntimeException(s"Invalid line: $line")
 
-def execute(instructions: Iterator[(Direction, Int)]): Iterator[Pos] =
+def execute(instructions: Iterator[(Pos, Int)]): Iterator[Pos] =
   var head_pos = (0, 0)
   var relative_tail_pos = (0, 0)
 
-  instructions.flatMap((d, steps) =>
-    val offset = d.offset
-    (1 to steps).map{ _ =>
-      head_pos = head_pos + offset
-      relative_tail_pos = follow(relative_tail_pos, offset)
-      head_pos + relative_tail_pos
-    }
-  )
+  for case (offset, steps) <- instructions
+           _ <- 1 to steps yield
+    head_pos = head_pos + offset
+    relative_tail_pos = follow(relative_tail_pos, offset)
+    head_pos + relative_tail_pos
 
 def follow(relative_tail_pos: Pos, offset: Pos): Pos =
-  // TH    ==>
+  // TH    =>
   //  TH
   // (-1, 0),  (1, 0)
   // (-1, 0)
@@ -72,6 +60,7 @@ def follow(relative_tail_pos: Pos, offset: Pos): Pos =
   // (-1, 0)
   else
     -offset
+
 
 @main def run_part1 =
   println(summarizeResource("day9_data.txt") { lines =>
